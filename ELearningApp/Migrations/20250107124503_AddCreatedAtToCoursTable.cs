19e7dc1f0f7d391603762fcdfdfc95412216cee5
MOHAMED EL MOSTAFI <elmoustafimohammed@gmail.com>
2025-01-07 22:25:20 +0100

    commit

---
+++ ELearningApp/Migrations/20250107124503_AddCreatedAtToCoursTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ELearningApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtToCoursTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Cours",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Abonnements",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Prix",
+                value: 499);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Cours");
+
+            migrationBuilder.UpdateData(
+                table: "Abonnements",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Prix",
+                value: 2800);
+        }
+    }
+}
